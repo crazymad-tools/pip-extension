695d6e7dcfcb8e52a964246597ef1d96298d8fe4
crazy_mad <crazymad0601@gmail.com>
2019-08-18 03:43:08 +0800

    添加egame支持

--- js/content.js
+++ js/content.js
@@ -1,14 +1,28 @@
 // console.log('I am PIP');
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'open-pip') {
+  let video = getVideo();
+  if (request.action === 'open-pip' && video) {
+    video[0].requestPictureInPicture();
+  } else if (request.action === 'close-pip' && video) {
+    video[0].exitPictureInPicture();
+  }
+})
+
+function getVideo () {
+  let url = document.location.href;
+  if (url.match(/^.*bilibili\.com\/video\/av[0-9]+$/i)) {
+    // bilibili
     let video = document.getElementsByTagName('video');
     if (video) {
       video[0].requestPictureInPicture();
     }
-  } else if (request.action === 'close-pip') {
-    let video = document.getElementById('video');
+    return video;
+  } else if (url.match(/^.*egame.qq.com\/[0-9]+$/i)) {
+    // egame
+    let video = document.getElementsByTagName('video');
     if (video) {
-      video[0].exitPictureInPicture();
+      video[0].requestPictureInPicture();
     }
+    return video;
   }
-})
+}
